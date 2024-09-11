8b535caa97330d3ec1c403c2f51456b2b97139bd
Maxence_Sanchez <maxence.sanchez-varas-leclercq@uha.fr>
2024-09-11 09:05:27 +0200

    Initial commit

---
+++ script/script.js
@@ -0,0 +1,28 @@
+
+document.querySelector("h1>span").addEventListener('click', chat)
+
+function chat(event){
+    console.log(event)
+    console.log(this.innerText)
+}
+
+
+document.querySelector("h1").addEventListener('click', air)
+
+function air(){
+    console.log('i am atomique')
+}
+
+document.querySelector('.gros').addEventListener('click', balise)
+
+function balise(){
+    console.log(this.innerText)
+}
+
+window.addEventListener('click', oui)
+
+function oui(event){
+    let posiX = event.pageX || event.changedTouches[0].pageX;
+    console.log(posiX)
+}
+
